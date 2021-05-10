e496cf9030f262562669c3b96f859fdadbdb301f
huajiaohuixiang <1751415583@qq.com>
2021-05-10 15:55:45 +0800

    删除后的

--- src/main/java/com/ytooo/model/Movie.java
+++ src/main/java/com/ytooo/model/Movie.java
@@ -13,9 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Movie {
-//	private  static double  leastToTopMovie=50;
-//	private static double allMovieMeanScore=3.328;
-
+	private  static double  leastToTopMovie=50;
+	private static double allMovieMeanScore=3.328;
+	double WRscore;
+	public double CalcuWRScore(){
+		WRscore=(voteCount)*voteAverage/(voteCount+leastToTopMovie)+(leastToTopMovie)*allMovieMeanScore/(voteCount+leastToTopMovie);
+		return WRscore;
+	}
 	@CsvBindByName(column = "budget")
 	int	budget;
 	@CsvBindByName(column = "genres")
@@ -54,11 +58,142 @@ public class Movie {
 
 	@CsvBindByName(column = "title")
 	String title;
+
+
+
 	@CsvBindByName(column = "vote_average")
-	int voteAverage;
+	double voteAverage;
 
 	@CsvBindByName(column = "vote_count")
 	int voteCount;
+
+	public int getBudget() {
+		return budget;
+	}
+
+	public void setBudget(int budget) {
+		this.budget = budget;
+	}
+
+	public String getGenres() {
+		return genres;
+	}
+
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getOriginalLanguage() {
+		return originalLanguage;
+	}
+
+	public void setOriginalLanguage(String originalLanguage) {
+		this.originalLanguage = originalLanguage;
+	}
+
+	public String getOriginalTitle() {
+		return originalTitle;
+	}
+
+	public void setOriginalTitle(String originalTitle) {
+		this.originalTitle = originalTitle;
+	}
+
+	public String getPopularity() {
+		return popularity;
+	}
+
+	public void setPopularity(String popularity) {
+		this.popularity = popularity;
+	}
+
+	public String getProductionCompanies() {
+		return productionCompanies;
+	}
+
+	public void setProductionCompanies(String productionCompanies) {
+		this.productionCompanies = productionCompanies;
+	}
+
+	public String getProductionCountries() {
+		return productionCountries;
+	}
+
+	public void setProductionCountries(String productionCountries) {
+		this.productionCountries = productionCountries;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public int getRevenue() {
+		return revenue;
+	}
+
+	public void setRevenue(int revenue) {
+		this.revenue = revenue;
+	}
+
+	public int getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+
+	public String getSpokenLanguages() {
+		return spokenLanguages;
+	}
+
+	public void setSpokenLanguages(String spokenLanguages) {
+		this.spokenLanguages = spokenLanguages;
+	}
+
+	public String getTagline() {
+		return tagline;
+	}
+
+	public void setTagline(String tagline) {
+		this.tagline = tagline;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getVoteAverage() {
+		return voteAverage;
+	}
+
+	public void setVoteAverage(double voteAverage) {
+		this.voteAverage = voteAverage;
+	}
+
+	public int getVoteCount() {
+		return voteCount;
+	}
+
+	public void setVoteCount(int voteCount) {
+		this.voteCount = voteCount;
+	}
 
 //	@CsvBindByName(column = "cast")
 //	String cast;
--- src/test/java/com/ytooo/DroolsApplicationTests.java
+++ src/test/java/com/ytooo/DroolsApplicationTests.java
@@ -5,6 +5,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 import com.thoughtworks.xstream.core.BaseException;
 import com.ytooo.bean.*;
+import com.ytooo.model.Movie;
 import com.ytooo.utils.csvUtils;
 import org.apache.poi.ss.formula.functions.T;
 import org.junit.jupiter.api.AfterEach;
@@ -50,9 +51,9 @@ class DroolsApplicationTests {
     @Test
     public  void getScore(){
         Movie movie=new Movie();
-        movie.setMovieId("123");
-        movie.setMeanscore(8.99);
-        movie.setVoteNumber(2000);
+        movie.setId(1);
+        movie.setVoteAverage(8.99);
+        movie.setVoteCount(2000);
         session.insert(movie);
         session.fireAllRules();
 
@@ -62,76 +63,6 @@ class DroolsApplicationTests {
 
     }
 
-
-    @Test
-    public void people() {
-
-        People people = new People();
-        people.setName("达");
-        people.setSex(1);
-        people.setDrlType("people");
-        session.insert(people);//插入
-        session.fireAllRules();//执行规则
-    }
-
-    @Test
-    public void cat() {
-        Cat cat = new Cat();
-        cat.setName("金");
-        cat.setSex(1);
-        session.insert(cat);//插入
-        session.fireAllRules();//执行规则
-    }
-
-    @Test
-    public void from() {
-        People p1 = new People(1, "达", "from");
-        People p2 = new People(0, "秋", "from");
-        People p3 = new People(3, "金", "from");
-        Animal animal = new Animal();
-        animal.setPeoples(new ArrayList<>());
-        animal.getPeoples().add(p1);
-        animal.getPeoples().add(p2);
-        animal.getPeoples().add(p3);
-        session.insert(animal);//插入
-        session.fireAllRules();//执行规则
-    }
-
-    @Test
-    public void collect() {
-        session.insert(new People(1, "达", "collect"));
-        session.insert(new People(0, "秋", "collect"));
-        session.insert(new People(0, "春", "collect"));
-        session.insert(new People(1, "夏", "collect"));
-        session.insert(new People(0, "冬", "collect"));
-        session.insert(new People(3, "金", "collect"));
-
-        session.fireAllRules();//执行规则
-    }
-
-    @Test
-    public void accumulate() {
-        session.insert(new Sensor("达", 8.26));
-        session.insert(new Sensor("秋", 7.12));
-        session.insert(new Sensor("春", 3.24));
-        session.insert(new Sensor("夏", 6.32));
-        session.insert(new Sensor("冬", 12.23));
-        session.insert(new Sensor("金", 10.8));
-
-        session.fireAllRules();//执行规则
-    }
-
-    @Test
-    public void diyaccumulate() {
-        session.insert(new People(1, "达", 26, "diyaccumulate"));
-        session.insert(new People(0, "秋", 18, "diyaccumulate"));
-        session.insert(new People(0, "春", 38, "diyaccumulate"));
-        session.insert(new People(1, "夏", 90, "diyaccumulate"));
-        session.insert(new People(0, "冬", 55, "diyaccumulate"));
-        session.insert(new People(3, "金", 12, "diyaccumulate"));
-
-        session.fireAllRules();//执行规则
-    }
 
     @AfterEach
     public void runDispose() {
